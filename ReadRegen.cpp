cd9414aa155019139bd588e75c26fe872b2d1771
Katzenbiber <Katzenbiber@users.noreply.github.com>
2016-10-20 18:04:23 +0200

    Fixed Pipes; Added Time

--- ReadRegen.cpp
+++ ReadRegen.cpp
@@ -1,3 +1,4 @@
+//Copyright 1000% @ Christopher Wrobel //2000% @David Berger // unendlich% @Christopher Wrobel
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -9,30 +10,37 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <iostream>
 
-using namespace std;
 
+using namespace std;
   
 int main(int argc, char *argv[])
 {
-  int fd, n, i, pipe;
+  int fd, n, i, pipe1,pipe2;
   char buf[6];
   struct termios toptions;
-  const char * RegenRinne = "/tmp/RegenRinne";
+  const char * RegenRinne1 = "/tmp/RegenRinne1";
+  const char * RegenRinne2 = "/tmp/RegenRinne2";
   double Rain;
   int rect;
+  char receive;
   
-  mkfifo(RegenRinne, 0666);
-  pipe = open(RegenRinne, O_WRONLY);
-
-  /* open serial port */
+  mkfifo(RegenRinne1, 0666);
+  mkfifo(RegenRinne2, 0666);
+  pipe1 = open(RegenRinne1, O_RDWR);
+  pipe2 = open(RegenRinne2, O_RDWR);
+  /* Öffnen des Ports zum Arduino (Seriell) */
   fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY);
   printf("fd opened as %i\n", fd);
   
-  /* get current serial port settings */
+  /* Abfragen der aktuellen Seriell-Port Einstellungen */
   tcgetattr(fd, &toptions);
-  /* set 9600 baud both ways */
+  /* Baudrate 9600; Bidirektional */
   cfsetispeed(&toptions, B9600);
   cfsetospeed(&toptions, B9600);
   /* 8 bits, no parity, no stop bits */
@@ -42,28 +50,28 @@ int main(int argc, char *argv[])
   toptions.c_cflag |= CS8;
   /* Canonical mode */
   toptions.c_lflag |= ICANON;
-  /* commit the serial port settings */
+  /* Abschicken der Optionen */
   tcsetattr(fd, TCSANOW, &toptions);
 
-  /* Receive string from Arduino */
-  do{
-  n = read(fd, buf, 6);
-  /* insert terminating zero in the string */
+  while(1){
+	  
+  read(pipe1,&receive,1); //Warten auf Anweisung vom Server Daten zu lesen
+  write(fd, "A",1); //Daten abfragen
+    n = read(fd, buf, 6);
+  /* 0 als Abschluss zum String hinzufügen */
   buf[n] = 0;
   
-  sscanf(buf, "%d", &rect);
-  Rain = 1-(double)rect/1024.0;
+  sscanf(buf, "%d", &rect); //Zusammenbauen der Empfangenen Daten
+  Rain = 1-(double)rect/1024.0; //Umrechnen zu Prozent
   
-  cout << rect << endl;
-  cout << Rain << endl;
+  cout <<currentTime()<< rect << endl; //Debug Output
+  cout <<currentTime()<< Rain << endl;
   
+  write(pipe2, (void *)&Rain, sizeof(Rain)); //Übertragung zum Server
   
-  write(pipe, (void *)&Rain, sizeof(Rain));
-  
-  
-  
-}while(1);
-close(pipe);
+}
+close(pipe1); // Schließen aller offenen Streams
+close(pipe2);
 close(fd);
 
   return 0;
