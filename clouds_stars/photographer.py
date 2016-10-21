5843f51153afacbe9d6f7c3f6771608274d6b969
ces42 <ca2010es@gmail.com>
2016-10-21 22:21:14 +0200

    Kommunikation mit Server

--- clouds_stars/photographer.py
+++ clouds_stars/photographer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# coding=utf-8
 #-------------------------------------------------------------------------------
 # Name:       photographer
 # Purpose:    
@@ -13,28 +14,38 @@ import picamera
 from time import sleep, time
 from io import BytesIO
 import PIL.Image as im
+import os
 
-
+PIPE_IN_NAME = '/tmp/camin'
+PIPE_OUT_NAME = '/tmp/camout'
 T = 1.0 # Zeitintervall, in dem Bilder gemacht werden
 
+os.mkfifo(PIPE_IN_NAME)
+os.mkfifo(PIPE_OUT_NAME)
 
-while True: #TODO: hier sollten wir vielleicht Uhrzeiten abfragen
-    stream = BytesIO() # wir emulieren eine Datei
-    t = time()
 
-    # das with-statement sagt Python es soll sich um das schließen von devices etc. kümmern
-    with picamera.PiCamera() as camera:
-        # einstellungen für Photographie mit wenig Licht
-        camera.framerate = 1/4
-        camera.shutter_speed = 4000000
-        camera.exposure_mode = 'off'
-        camera.iso = 600
-        camera.capture(stream, format='jpeg') # Photo schießen
+with open(PIPE_IN_NAME, 'rb') as pipe_in, open(PIPE_OUT_NAME, 'wb') as pipe_out:
+    while True: #TODO: hier sollten wir vielleicht Uhrzeiten abfragen
+        stream = BytesIO() # wir emulieren eine Datei
+        t = time()
 
-    image = im.open(stream.getvalue())
-    print(clouded(image))
+        # das with-statement sagt Python es soll sich um das schließen von devices etc. kümmern
+        with picamera.PiCamera() as camera:
+            # einstellungen für Photographie mit wenig Licht
+            camera.framerate = 1/4
+            camera.shutter_speed = 4000000
+            camera.exposure_mode = 'off'
+            camera.iso = 600
+            camera.capture(stream, format='jpeg') # Photo schießen
 
-    sleep(T + t - time()) # jeder Durchgang der Schleife soll  T minuten dauern
+        image = im.open(stream.getvalue())
+        cloud_cover = clouded(image)
+
+        pipe_in.read() # warten auf Anfrage
+        pipe_out.write( b'{:032b}'.format(int(4 * cloud_cover)) )
+
+
+        sleep(T + t - time()) # jeder Durchgang der Schleife soll mindestens T minuten dauern
 
 
 
