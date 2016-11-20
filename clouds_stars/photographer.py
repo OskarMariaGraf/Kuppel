5c9ca7c739485b340738dd55eb6943380a6dda75
Katzenbiber <wrobel1999@gmail.com>
2016-11-20 15:29:51 +0100

    restrukturiert

--- clouds_stars/photographer.py
+++ clouds_stars/photographer.py
@@ -20,7 +20,9 @@ PIPE_OUT_NAME = '/tmp/camout'
 
 T = 1.0 # Zeitintervall, in dem Bilder gemacht werden
 
-logging.info('lösche alte pipes und erstelle neue')
+logging.root.setLevel(logging.DEBUG)
+
+logging.debug('lösche alte pipes und erstelle neue')
 
 os.remove(PIPE_IN_NAME)
 os.remove(PIPE_OUT_NAME)
@@ -30,9 +32,15 @@ os.mkfifo(PIPE_OUT_NAME)
 
 # die Pipes werden zur Kommunikation mit dem Server verwendet - vgl. Server.cpp
 with open(PIPE_IN_NAME, 'rb') as pipe_in, open(PIPE_OUT_NAME, 'wb') as pipe_out:
+    logging.debug('Pipes erstellt und geöfnet')
+
     while True: #TODO: hier sollten wir vielleicht Uhrzeiten abfragen
-        stream = BytesIO() # wir emulieren eine Datei
         t = time()
+
+        pipe_in.read() # warten auf Anfrage
+        logging.info('Anfrage von Server erhalten')
+
+        stream = BytesIO() # wir emulieren eine Datei
 
         # das with-statement sagt Python es soll sich um das schließen von devices etc. kümmern
         with picamera.PiCamera() as camera:
@@ -47,14 +55,11 @@ with open(PIPE_IN_NAME, 'rb') as pipe_in, open(PIPE_OUT_NAME, 'wb') as pipe_out:
         image = im.open(stream.getvalue())
         gray = np.asarray(image.convert('LA'))[..., 0]
         cloud_cover = clouded(gray)
-        logging.info('Cloudcover beträgt {}')
-
-        pipe_in.read() # warten auf Anfrage
-        logging.info('Anfrage von Server erhalten')
+        logging.debug('Cloudcover beträgt {}')
 
         pipe_out.write( bytes([0, 0, 0, int(100 * cloud_cover)]) )
         logging.info('Cloudcover-Wert {} an Server geschickt'.format(100*cloud_cover))
-
+        
         sleep(T + t - time()) # jeder Durchgang der Schleife soll mindestens T minuten dauern
 
 
