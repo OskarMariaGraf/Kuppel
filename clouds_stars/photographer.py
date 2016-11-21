2cc7dee40761f899b8a6c36dead0e2a200cdd6b5
ces42 <ca2010es@gmail.com>
2016-11-21 23:06:06 +0100

    ganzzahlige Blichtungszeit

--- clouds_stars/photographer.py
+++ clouds_stars/photographer.py
@@ -92,12 +92,12 @@ def eval_sky():
         # falls das Bild zu hell ist wird die Belichtungszeit reduziert
         # helligkeitswerte zwischen 20 und 40 werden angepeilt
         if helligkeit > 100:
-            shutter_speed *= 50 / helligkeit # Notbremse
+            shutter_speed = int(shutter_speed * 50 / helligkeit) # Notbremse
         elif helligkeit > 45:
-            shutter_speed = max(shutter_speed * 0.8, 1)
+            shutter_speed = max(int(shutter_speed * 0.8), 1)
             logger.info('Beleuchtungszeit auf {} ms heruntergesetzt'.format(shutter_speed))
         elif helligkeit < 15 and shutter_speed < BASE_SS:
-            shutter_speed = min(BASE_SS, shutter_speed * 1.2)
+            shutter_speed = min(BASE_SS, int(shutter_speed * 1.2))
             logger.info('Beleuchtungszeit auf {} ms heraufgesetzt'.format(shutter_speed))
 
 
